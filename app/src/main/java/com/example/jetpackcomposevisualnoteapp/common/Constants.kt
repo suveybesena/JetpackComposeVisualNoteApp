16329048eaa1ce191fd7e5c6005ab6dc5fe90782
suveybesena <suveybesena@gmail.com>
2022-05-26 23:41:59 +0300

    create constants

---
+++ app/src/main/java/com/example/jetpackcomposevisualnoteapp/common/Constants.kt
@@ -0,0 +1,15 @@
+package com.example.jetpackcomposevisualnoteapp.common
+
+object Constants {
+    const val TABLE_NAME = "notes"
+    const val LOCAL_DB_NOTE_URL = "noteUrl"
+    const val LOCAL_DB_NOTE_TITLE = "noteTitle"
+    const val LOCAL_DB_NOTE_DESC = "noteDesc"
+    const val LOCAL_DB_NOTE_ID = "id"
+    const val LOCAL_DB_NOTE_DATE = "date"
+    const val EDITED_TAG = "edited"
+    const val EDITED_TAG_TITLE = "Edited"
+    const val DB_NAME = "notes_db"
+    const val NULL_ERROR_MESSAGE = "This fields cannot be empty."
+    const val PARCELABLE_ARGS_ID = "noteDetail"
+}
