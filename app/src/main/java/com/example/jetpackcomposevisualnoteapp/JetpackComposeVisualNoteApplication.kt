829c019068da34925d6fec22b41e81fae590e27c
suveybesena <suveybesena@gmail.com>
2022-06-01 00:02:49 +0300

    add hilt application

---
+++ app/src/main/java/com/example/jetpackcomposevisualnoteapp/JetpackComposeVisualNoteApplication.kt
@@ -0,0 +1,8 @@
+package com.example.jetpackcomposevisualnoteapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class JetpackComposeVisualNoteApplication : Application() {
+}
---
+++ app/src/main/java/com/example/jetpackcomposevisualnoteapp/presentation/addnote/AddNoteViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.jetpackcomposevisualnoteapp.presentation.addnote
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.jetpackcomposevisualnoteapp.common.Resource
+import com.example.jetpackcomposevisualnoteapp.data.model.NoteModel
+import com.example.jetpackcomposevisualnoteapp.domain.usecases.AddNoteToLocalUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AddNoteViewModel @Inject constructor(private val addNoteToLocalUseCase: AddNoteToLocalUseCase) :
+    ViewModel() {
+
+    private val _uiState = mutableStateOf(AddNoteUiState())
+    val uiState: State<AddNoteUiState> = _uiState
+
+    fun handleEvent(event: AddNoteUiEvent) {
+        when (event) {
+            is AddNoteUiEvent.AddNote -> {
+                addNote(event.noteDetail)
+            }
+        }
+    }
+
+    private fun addNote(noteDetail: NoteModel) {
+        viewModelScope.launch {
+            addNoteToLocalUseCase.invoke(noteDetail).collect { resource ->
+                when (resource) {
+                    is Resource.Error -> {
+                        _uiState.value = uiState.value.copy(error = resource.message)
+                    }
+                    is Resource.Loading -> {
+                        _uiState.value = uiState.value.copy(isLoading = true)
+                    }
+                }
+            }
+        }
+    }
+}
