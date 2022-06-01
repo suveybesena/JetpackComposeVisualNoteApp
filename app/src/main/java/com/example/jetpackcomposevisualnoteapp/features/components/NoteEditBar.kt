ddaef84adc0921953304b8a6de0271e59cb17e50
suveybesena <suveybesena@gmail.com>
2022-06-02 00:06:03 +0300

    replace editbar to components

---
+++ app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/components/NoteEditBar.kt
@@ -0,0 +1,57 @@
+package com.example.jetpackcomposevisualnoteapp.features.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NoteEditBar(
+    initialText: String,
+    modifier: Modifier = Modifier,
+    hint: String = "",
+    onChanged: (String) -> Unit = {}
+) {
+    var text by remember {
+        mutableStateOf(initialText)
+    }
+    var isHintDisplayed by remember {
+        mutableStateOf(hint != "")
+    }
+    Box(modifier = modifier) {
+        BasicTextField(value = text, onValueChange = {
+            text = it
+            onChanged(it)
+        },
+            maxLines = 1,
+            singleLine = true,
+            textStyle = TextStyle(color = Color.Black),
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(5.dp, CircleShape)
+                .background(Color.White, CircleShape)
+                .padding(horizontal = 20.dp, vertical = 12.dp)
+                .onFocusChanged {
+                    isHintDisplayed = it.isFocused != true && text.isEmpty()
+                }
+        )
+        if (isHintDisplayed) {
+            Text(
+                text = hint,
+                color = Color.Gray,
+                modifier = Modifier
+                    .padding(horizontal = 20.dp, vertical = 12.dp)
+            )
+        }
+    }
+}
