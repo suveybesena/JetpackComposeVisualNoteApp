f21c76acbb56b279cb32eb77450f994aa9163275
suveybesena <suveybesena@gmail.com>
2022-06-03 01:05:51 +0300

    remove unused lines and imports

--- app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/screen/addnote/NoteAddScreen.kt
+++ app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/screen/addnote/NoteAddScreen.kt
@@ -1,10 +1,9 @@
-package com.example.jetpackcomposevisualnoteapp.presentation.addnote
+package com.example.jetpackcomposevisualnoteapp.features.screen.addnote
 
 
-import androidx.compose.foundation.background
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.ExtendedFloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Surface
@@ -13,18 +12,17 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.jetpackcomposevisualnoteapp.data.model.NoteModel
-import com.example.jetpackcomposevisualnoteapp.presentation.Screen
+import com.example.jetpackcomposevisualnoteapp.features.components.NoteAddBar
+import com.example.jetpackcomposevisualnoteapp.features.navigation.Screen
 
 @Composable
 fun NoteAddScreen(
@@ -48,13 +46,12 @@ fun NoteAddScreen(
             Text(
                 "Add Notes", modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 20.dp),
-                textAlign = TextAlign.Center,
+                    .padding(20.dp),
+                textAlign = TextAlign.Start,
                 fontSize = 30.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color.Gray
             )
-            Spacer(modifier = Modifier.height(10.dp))
             NoteAddBar(
                 hint = "Note Title",
                 modifier = Modifier
@@ -116,44 +113,23 @@ fun FloatingButton(
     )
 }
 
+
+@Preview(showBackground = true)
 @Composable
-fun NoteAddBar(
-    modifier: Modifier = Modifier,
-    hint: String = "",
-    onChanged: (String) -> Unit = {}
-) {
-    var text by remember {
-        mutableStateOf("")
-    }
-    var isHintDisplayed by remember {
-        mutableStateOf(hint != "")
-    }
-    Box(modifier = modifier) {
-        BasicTextField(value = text, onValueChange = {
-            text = it
-            onChanged(it)
+fun FloatingButton() {
+    ExtendedFloatingActionButton(
+        text =
+        { Text(text = "Add") },
+        icon = { Icon(Icons.Filled.Add, "ExtendedFloatingActionButton") },
+        onClick = {
+            Log.v("FloatingActionButton", "Clicked")
         },
-            maxLines = 1,
-            singleLine = true,
-            textStyle = TextStyle(color = Color.Black),
-            modifier = Modifier
-                .fillMaxWidth()
-                .shadow(5.dp, CircleShape)
-                .background(Color.White, CircleShape)
-                .padding(horizontal = 20.dp, vertical = 12.dp)
-                .onFocusChanged {
-                    isHintDisplayed = it.isFocused != true && text.isEmpty()
-                }
-        )
-        if (isHintDisplayed) {
-            Text(
-                text = hint,
-                color = Color.Gray,
-                modifier = Modifier
-                    .padding(horizontal = 20.dp, vertical = 12.dp)
-            )
-        }
-    }
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    )
 }
+
+
 
 
