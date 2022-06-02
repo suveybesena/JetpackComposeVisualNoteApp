6051d36218a1f38408309dbfb0c4bdf04f233431
suveybesena <suveybesena@gmail.com>
2022-06-03 01:06:34 +0300

    add remember state

--- app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/screen/editnote/NoteEditScreen.kt
+++ app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/screen/editnote/NoteEditScreen.kt
@@ -1,4 +1,4 @@
-package com.example.jetpackcomposevisualnoteapp.presentation.edit
+package com.example.jetpackcomposevisualnoteapp.features.screen.editnote
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.ExtendedFloatingActionButton
@@ -7,7 +7,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -16,18 +16,27 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.jetpackcomposevisualnoteapp.common.Constants
 import com.example.jetpackcomposevisualnoteapp.data.model.NoteModel
-import com.example.jetpackcomposevisualnoteapp.presentation.addnote.NoteAddBar
+import com.example.jetpackcomposevisualnoteapp.features.components.NoteEditBar
+import com.example.jetpackcomposevisualnoteapp.features.navigation.Screen
 
 @Composable
 fun NoteEditScreen(
     navController: NavController,
+    navArg: NoteModel,
     viewModel: NoteEditViewModel = hiltViewModel()
-) {
 
-    lateinit var noteTitle: String
-    var noteDesc = ""
-    var noteImageUrl = ""
+) {
+    var noteTitle by remember {
+        mutableStateOf(navArg.noteTitle)
+    }
+    var noteDesc by remember {
+        mutableStateOf(navArg.noteDesc)
+    }
+    var noteImageUrl by remember {
+        mutableStateOf(navArg.imageUrl)
+    }
     Surface(
         color = Color.White,
         modifier = Modifier.fillMaxWidth()
@@ -36,36 +45,36 @@ fun NoteEditScreen(
             Text(
                 "Edit Notes", modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 20.dp),
-                textAlign = TextAlign.Center,
+                    .padding(20.dp),
+                textAlign = TextAlign.Start,
                 fontSize = 30.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color.Gray
             )
             Spacer(modifier = Modifier.height(10.dp))
-            NoteAddBar(
+            NoteEditBar(
                 hint = "Note Title",
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp)
+                    .padding(16.dp), initialText = navArg.noteTitle!!
             ) {
                 noteTitle = it
             }
             Spacer(modifier = Modifier.height(10.dp))
-            NoteAddBar(
+            NoteEditBar(
                 hint = "Note Description",
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp)
+                    .padding(16.dp), initialText = navArg.noteDesc!!
             ) {
                 noteDesc = it
             }
             Spacer(modifier = Modifier.height(10.dp))
-            NoteAddBar(
+            NoteEditBar(
                 hint = "Note Image Url",
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp)
+                    .padding(16.dp), initialText = navArg.imageUrl!!
             ) {
                 noteImageUrl = it
             }
@@ -76,14 +85,17 @@ fun NoteEditScreen(
                 icon = { Icon(Icons.Filled.Add, "") },
                 onClick = {
                     val date = System.currentTimeMillis()
+                    val editedTag = Constants.EDITED_TAG_TITLE
                     val noteModel = NoteModel(
-                        "https://www.ikea.com/ca/en/images/products/fejka-artificial-potted-plant-indoor-outdoor-monstera__0614197_pe686822_s5.jpg?f=s",
+                        noteImageUrl,
                         date,
-                        "noteTitle",
-                        "noteDesc"
+                        noteTitle,
+                        noteDesc,
+                        editedTag,
+                        navArg.id
                     )
                     viewModel.handleEvent(EditNoteUiEvent.UpdateNote(noteModel))
-                    navController.navigate("note_list_screen")
+                    navController.navigate(Screen.NoteListScreen.route)
                 },
                 modifier = Modifier
                     .fillMaxWidth()
@@ -95,10 +107,16 @@ fun NoteEditScreen(
                 { Text(text = "Delete Note") },
                 icon = { Icon(Icons.Filled.Add, "") },
                 onClick = {
-                    // val date = System.currentTimeMillis()
-                    // val noteModel = NoteModel(noteImageUrl, date, noteTitle, noteDesc)
-                    // viewModel.handleEvent(EditNoteUiEvent.DeleteNote(noteModel))
-                    navController.navigate("note_list_screen")
+                    val date = System.currentTimeMillis()
+                    val noteModel = NoteModel(
+                        noteImageUrl,
+                        date,
+                        noteTitle,
+                        noteDesc,
+                        id = navArg.id
+                    )
+                    viewModel.handleEvent(EditNoteUiEvent.DeleteNote(noteModel))
+                    navController.navigate(route = Screen.NoteListScreen.route)
                 },
                 modifier = Modifier
                     .fillMaxWidth()
@@ -107,4 +125,5 @@ fun NoteEditScreen(
         }
     }
 }
+
 
