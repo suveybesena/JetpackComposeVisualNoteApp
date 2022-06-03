0c0d8a8746697f63d306345966b7399c93e731d4
suveybesena <suveybesena@gmail.com>
2022-06-04 00:05:46 +0300

    add time string

--- app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/components/NoteItemRow.kt
+++ app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/components/NoteItemRow.kt
@@ -11,34 +11,40 @@ import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposevisualnoteapp.R
 import com.example.jetpackcomposevisualnoteapp.data.model.NoteModel
-import com.example.jetpackcomposevisualnoteapp.features.components.NoteImage
 import com.example.jetpackcomposevisualnoteapp.ui.theme.Color
+import java.text.DateFormat
+import java.util.*
 
 @ExperimentalFoundationApi
 @Composable
 fun NoteItemRow(
     noteDetail: NoteModel,
-    onNoteClicked: (NoteModel?) -> Unit,
+    onNoteClicked: (NoteModel?) -> Unit
 ) {
     var isShortText by remember { mutableStateOf(false) }
-    val shortText = "Long press for detailed view +"
+    val shortText = "Long press for detailed view."
     val longText = noteDetail.noteDesc
 
     var isGray by remember { mutableStateOf(false) }
     val animateColor = animateColorAsState(
-        if (isGray) Color.Blue else androidx.compose.ui.graphics.Color.White,
+        if (isGray) Color.Highlight else androidx.compose.ui.graphics.Color.White,
         animationSpec = tween(durationMillis = 1500)
     )
 
-    Card(modifier = Modifier.padding(5.dp)) {
+    Card(
+        modifier = Modifier
+            .padding(5.dp)
+    ) {
         Row(modifier = Modifier
             .fillMaxWidth()
             .background(animateColor.value)
@@ -60,30 +66,66 @@ fun NoteItemRow(
                 placeholder = R.drawable.ic_error,
                 contentScale = ContentScale.Crop,
             )
-            Spacer(modifier = Modifier.width(5.dp))
+            Spacer(modifier = Modifier.width(10.dp))
             Box {
-                Column {
+                Column(modifier = Modifier.fillMaxHeight()) {
                     noteDetail.noteTitle?.let {
                         Text(
                             text = it,
-                            style = MaterialTheme.typography.h5,
-                            modifier = Modifier.padding(2.dp),
+                            fontSize = 15.sp,
+                            modifier = Modifier
+                                .padding(2.dp),
                             fontWeight = FontWeight.Bold
                         )
                     }
-                    Spacer(modifier = Modifier.height(10.dp))
+
+                    Spacer(modifier = Modifier.padding(5.dp))
+
                     noteDetail.noteDesc?.let {
                         (if (isShortText) longText else shortText)?.let { noteDesc ->
                             Text(
                                 text = noteDesc,
                                 style = MaterialTheme.typography.subtitle1,
-                                modifier = Modifier.padding(2.dp),
+                                modifier = Modifier
+                                    .padding(2.dp),
                                 fontWeight = FontWeight.Normal
                             )
                         }
                     }
                 }
             }
+            Box() {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.Bottom,
+                    horizontalAlignment = Alignment.End
+                ) {
+                    Box(contentAlignment = Alignment.TopEnd) {
+                        val calendar = Calendar.getInstance()
+                        val timeMillis = noteDetail.date
+                        calendar.timeInMillis = timeMillis
+                        Text(
+                            text = DateFormat.getDateInstance().format(calendar.time).toString(),
+                            fontSize = 10.sp,
+                            modifier = Modifier
+                                .padding(2.dp),
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                    Box(contentAlignment = Alignment.TopEnd) {
+                        Text(
+                            text = "${noteDetail.noteHour}:${noteDetail.noteMin}",
+                            fontSize = 10.sp,
+                            modifier = Modifier
+                                .padding(2.dp),
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                }
+            }
         }
     }
 }
+
+
