9faa7b014f38b239b0c59b06ebb849cbb38ea2b6
suveybesena <suveybesena@gmail.com>
2022-06-04 00:04:32 +0300

    made string constant

--- app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/navigation/NavGraph.kt
+++ app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/navigation/NavGraph.kt
@@ -4,10 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.jetpackcomposevisualnoteapp.common.Constants.PARCELABLE_ARGS_ID
 import com.example.jetpackcomposevisualnoteapp.data.model.NoteModel
 import com.example.jetpackcomposevisualnoteapp.features.screen.addnote.NoteAddScreen
 import com.example.jetpackcomposevisualnoteapp.features.screen.editnote.NoteEditScreen
-import com.example.jetpackcomposevisualnoteapp.features.navigation.Screen
 import com.example.jetpackcomposevisualnoteapp.features.screen.home.NoteListScreen
 import com.example.jetpackcomposevisualnoteapp.features.screen.splash.AnimatedSplashScreen
 
@@ -26,7 +26,9 @@ fun NavGraph() {
         }
         composable(route = Screen.NoteEditScreen.route) {
             val noteArg =
-                navController.previousBackStackEntry?.savedStateHandle?.get<NoteModel>("noteModelArg")
+                navController.previousBackStackEntry?.savedStateHandle?.get<NoteModel>(
+                    PARCELABLE_ARGS_ID
+                )
             if (noteArg != null) {
                 NoteEditScreen(navController = navController, noteArg)
             }
