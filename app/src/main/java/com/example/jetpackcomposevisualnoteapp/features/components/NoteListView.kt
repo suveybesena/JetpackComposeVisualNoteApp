8188233798d8f73b45995b6283d1c42d6388e4c0
suveybesena <suveybesena@gmail.com>
2022-06-04 01:28:40 +0300

    add itemsIndexed

--- app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/components/NoteListView.kt
+++ app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/components/NoteListView.kt
@@ -4,25 +4,34 @@ package com.example.jetpackcomposevisualnoteapp.features.components
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.jetpackcomposevisualnoteapp.data.model.NoteModel
 import com.example.jetpackcomposevisualnoteapp.features.navigation.Screen
+import com.example.jetpackcomposevisualnoteapp.features.screen.addnote.FloatingButton
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun NoteListView(notes: List<NoteModel>, navController: NavController) {
-
+fun NoteListView(
+    notes: List<NoteModel>,
+    navController: NavController
+) {
     LazyColumn(
         contentPadding = PaddingValues(5.dp)
-    ) {
-        items(notes) { notes ->
+    )
+    {
+        itemsIndexed(notes) { index, item ->
+
             NoteItemRow(
-                noteDetail = notes,
+                noteDetail = item,
                 onNoteClicked = { note ->
-                    navController.currentBackStackEntry?.savedStateHandle?.set("noteModelArg", note)
+                    navController.currentBackStackEntry?.savedStateHandle?.set(
+                        "noteModelArg",
+                        note
+                    )
                     navController.navigate(Screen.NoteEditScreen.route)
                 }
             )
