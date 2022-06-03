f00ec1e0a57de657a31dade40a001e7900a2e487
suveybesena <suveybesena@gmail.com>
2022-06-04 01:29:51 +0300

    change notelist  var

--- app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/screen/home/NotesListUiState.kt
+++ app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/screen/home/NotesListUiState.kt
@@ -5,5 +5,5 @@ import com.example.jetpackcomposevisualnoteapp.data.model.NoteModel
 data class NotesListUiState(
     val error: String? = null,
     val isLoading: Boolean? = null,
-    val notesList: List<NoteModel>? = null
+    var notesList: List<NoteModel>? = null
 )
