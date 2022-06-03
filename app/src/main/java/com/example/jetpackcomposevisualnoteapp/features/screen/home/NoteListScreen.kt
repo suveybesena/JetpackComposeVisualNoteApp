744ca994d9129da64b2ced4c0b98d368fd444336
suveybesena <suveybesena@gmail.com>
2022-06-04 00:06:04 +0300

    add time string

--- app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/screen/home/NoteListScreen.kt
+++ app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/screen/home/NoteListScreen.kt
@@ -1,13 +1,14 @@
 package com.example.jetpackcomposevisualnoteapp.features.screen.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExtendedFloatingActionButton
-import androidx.compose.material.Icon
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -15,32 +16,56 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.jetpackcomposevisualnoteapp.common.Constants.ADD_NOTE_SCREEN_TITLE
+import com.example.jetpackcomposevisualnoteapp.common.Constants.NOTES_LIST_SCREEN_TITLE
+import com.example.jetpackcomposevisualnoteapp.features.components.NoteListShimmer
 import com.example.jetpackcomposevisualnoteapp.features.components.NoteListView
 import com.example.jetpackcomposevisualnoteapp.features.navigation.Screen
 
 @Composable
 fun NoteListScreen(viewModel: NotesListViewModel = hiltViewModel(), navController: NavController) {
-
+    val noteListState = viewModel.uiState.value
     Column(
-        modifier = Modifier.fillMaxHeight(),
+        modifier = Modifier
+            .fillMaxHeight()
+            .background(color = com.example.jetpackcomposevisualnoteapp.ui.theme.Color.Gray),
         verticalArrangement = Arrangement.SpaceBetween
     ) {
         Column {
-            Text(
-                "Notes", modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(20.dp),
-                textAlign = TextAlign.Start,
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.Gray
-            )
-            NoteList(navController, viewModel)
+            Box(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .shadow(5.dp)
+                    .height(68.dp)
+                    .clip(shape = RoundedCornerShape(15))
+                    .background(com.example.jetpackcomposevisualnoteapp.ui.theme.Color.Blue)
+
+            ) {
+                Text(
+                    NOTES_LIST_SCREEN_TITLE,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp),
+                    textAlign = TextAlign.Start,
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+
+                    )
+            }
+
+            if (noteListState.isLoading == true) {
+                repeat(7) {
+                    NoteListShimmer()
+                }
+            } else {
+                NoteList(navController, noteListState)
+            }
+
         }
         ExtendedFloatingActionButton(
             text =
-            { Text(text = "Add Note") },
-            icon = { Icon(Icons.Filled.Add, "Add Note") },
+            { Text(text = ADD_NOTE_SCREEN_TITLE, color = Color.White) },
             onClick = {
                 navController.navigate(route = Screen.NoteAddScreen.route)
             },
@@ -52,9 +77,16 @@ fun NoteListScreen(viewModel: NotesListViewModel = hiltViewModel(), navControlle
 }
 
 @Composable
-fun NoteList(navController: NavController, viewModel: NotesListViewModel) {
-    val noteListState = viewModel.uiState.value
-    noteListState.notesList?.let { NoteListView(notes = it, navController = navController) }
+fun NoteList(
+    navController: NavController,
+    noteListState: NotesListUiState
+) {
+    noteListState.notesList?.let {
+        NoteListView(
+            notes = it,
+            navController = navController
+        )
+    }
 }
 
 
