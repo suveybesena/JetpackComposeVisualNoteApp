e23026144757023d2e5c4e5b9a6ca41bf4667a0e
suveybesena <suveybesena@gmail.com>
2022-06-05 16:19:42 +0300

    add navArgs

--- app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/components/NoteListView.kt
+++ app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/components/NoteListView.kt
@@ -5,19 +5,21 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.FloatingActionButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.jetpackcomposevisualnoteapp.data.model.NoteModel
 import com.example.jetpackcomposevisualnoteapp.features.navigation.Screen
-import com.example.jetpackcomposevisualnoteapp.features.screen.addnote.FloatingButton
+import com.example.jetpackcomposevisualnoteapp.features.screen.home.NotesListUiEvent
+import com.example.jetpackcomposevisualnoteapp.features.screen.home.NotesListViewModel
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun NoteListView(
     notes: List<NoteModel>,
-    navController: NavController
+    navController: NavController,
+    viewModel: NotesListViewModel = hiltViewModel()
 ) {
     LazyColumn(
         contentPadding = PaddingValues(5.dp)
@@ -32,7 +34,18 @@ fun NoteListView(
                         "noteModelArg",
                         note
                     )
+                    navController.navigate(Screen.NoteDetailScreen.route)
+                },
+                onEditSwipe = { note ->
+                    navController.currentBackStackEntry?.savedStateHandle?.set(
+                        "noteModelArg",
+                        note
+                    )
                     navController.navigate(Screen.NoteEditScreen.route)
+                },
+                onDeleteSwipe = {
+                    viewModel.handleEvent(NotesListUiEvent.DeleteNote(item))
+                    viewModel.handleEvent(NotesListUiEvent.GetAllNotes)
                 }
             )
         }
