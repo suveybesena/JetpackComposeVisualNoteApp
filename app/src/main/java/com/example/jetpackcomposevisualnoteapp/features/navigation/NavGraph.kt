186789479fe70d10b8b5c2e7e844f791d1abc9a9
suveybesena <suveybesena@gmail.com>
2022-06-05 16:17:53 +0300

    add screens to nav graph

--- app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/navigation/NavGraph.kt
+++ app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/navigation/NavGraph.kt
@@ -1,16 +1,27 @@
 package com.example.jetpackcomposevisualnoteapp.features.navigation
 
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.jetpackcomposevisualnoteapp.common.Constants.PARCELABLE_ARGS_ID
+import com.example.jetpackcomposevisualnoteapp.common.Constants.STRING_ARGS_ID
 import com.example.jetpackcomposevisualnoteapp.data.model.NoteModel
 import com.example.jetpackcomposevisualnoteapp.features.screen.addnote.NoteAddScreen
+import com.example.jetpackcomposevisualnoteapp.features.screen.drawing.DrawingScreen
 import com.example.jetpackcomposevisualnoteapp.features.screen.editnote.NoteEditScreen
 import com.example.jetpackcomposevisualnoteapp.features.screen.home.NoteListScreen
+import com.example.jetpackcomposevisualnoteapp.features.screen.main.MainActivity
+import com.example.jetpackcomposevisualnoteapp.features.screen.notedetail.NoteDetailScreen
 import com.example.jetpackcomposevisualnoteapp.features.screen.splash.AnimatedSplashScreen
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
 
+@OptIn(
+    ExperimentalAnimationApi::class, ExperimentalComposeUiApi::class,
+    ExperimentalPermissionsApi::class
+)
 @Composable
 fun NavGraph() {
     val navController = rememberNavController()
@@ -21,8 +32,26 @@ fun NavGraph() {
         composable(route = Screen.NoteListScreen.route) {
             NoteListScreen(navController = navController)
         }
+        composable(route = Screen.DrawingScreen.route) {
+            DrawingScreen(navController = navController)
+        }
+        composable(route = Screen.NoteDetailScreen.route) {
+            val noteArg =
+                navController.previousBackStackEntry?.savedStateHandle?.get<NoteModel>(
+                    PARCELABLE_ARGS_ID
+                )
+            if (noteArg != null) {
+                NoteDetailScreen(noteArg)
+            }
+        }
         composable(route = Screen.NoteAddScreen.route) {
-            NoteAddScreen(navController = navController)
+            val noteArg =
+                navController.previousBackStackEntry?.savedStateHandle?.get<String>(
+                    STRING_ARGS_ID
+                )
+            if (noteArg != null) {
+                NoteAddScreen(noteArg, navController = navController)
+            }
         }
         composable(route = Screen.NoteEditScreen.route) {
             val noteArg =
