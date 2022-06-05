a55e71dd31f3e89754755002dc2ec1c41a5c33df
suveybesena <suveybesena@gmail.com>
2022-06-05 16:16:44 +0300

    add colors

--- app/src/main/java/com/example/jetpackcomposevisualnoteapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/jetpackcomposevisualnoteapp/ui/theme/Color.kt
@@ -8,6 +8,7 @@ val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 val Pink = Color(0xFFD68680)
+val graySurface = Color(0xFF2A2A2A)
 
 
 class Color {
@@ -18,5 +19,9 @@ class Color {
         val Highlight = Color(0xFFBDDFD6)
         @Stable
         val Gray = Color(0xDAF7F7F7)
+        @Stable
+        val Red = Color(0xDAD80000)
+        @Stable
+        val Orange = Color(0xFFFCCF47)
     }
 }
