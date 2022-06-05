75bf2ad2218086c9612b3efc4428c6c8c34a2409
suveybesena <suveybesena@gmail.com>
2022-06-05 16:20:31 +0300

    remove delete use case

--- app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/screen/home/NotesListViewModel.kt
+++ app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/screen/home/NotesListViewModel.kt
@@ -5,20 +5,38 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.jetpackcomposevisualnoteapp.common.Resource
+import com.example.jetpackcomposevisualnoteapp.data.model.NoteModel
+import com.example.jetpackcomposevisualnoteapp.domain.usecases.DeleteNoteFromLocalUseCase
 import com.example.jetpackcomposevisualnoteapp.domain.usecases.GetAllNotesFromLocalUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class NotesListViewModel @Inject constructor(private val getAllNotesFromLocalUseCase: GetAllNotesFromLocalUseCase) :
+class NotesListViewModel @Inject constructor(
+    private val getAllNotesFromLocalUseCase: GetAllNotesFromLocalUseCase,
+    private val deleteNoteFromLocalUseCase: DeleteNoteFromLocalUseCase
+) :
     ViewModel() {
+
+    init {
+        getAllNotes()
+    }
+
 
     private val _uiState = mutableStateOf(NotesListUiState())
     val uiState: State<NotesListUiState> = _uiState
 
-    init {
-        getAllNotes()
+    fun handleEvent(event: NotesListUiEvent) {
+        when (event) {
+            is NotesListUiEvent.DeleteNote -> {
+                deleteNote(event.noteDetail)
+            }
+            is NotesListUiEvent.GetAllNotes -> {
+                getAllNotes()
+            }
+
+        }
     }
 
     private fun getAllNotes() {
@@ -35,6 +53,21 @@ class NotesListViewModel @Inject constructor(private val getAllNotesFromLocalUse
                     }
                     is Resource.Error -> {
                         _uiState.value = uiState.value.copy(error = resource.message)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun deleteNote(noteDetail: NoteModel) {
+        viewModelScope.launch {
+            deleteNoteFromLocalUseCase.invoke(noteDetail).collect { resource ->
+                when (resource) {
+                    is Resource.Error -> {
+                        _uiState.value = uiState.value.copy(error = resource.message)
+                    }
+                    is Resource.Loading -> {
+                        _uiState.value = uiState.value.copy(isLoading = true)
                     }
                 }
             }
