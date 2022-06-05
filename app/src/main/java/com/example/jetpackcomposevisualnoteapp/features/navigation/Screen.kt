d473a8dcbde4775d349d548609df4aec2c7f07f8
suveybesena <suveybesena@gmail.com>
2022-06-05 16:20:40 +0300

    add screens

--- app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/navigation/Screen.kt
+++ app/src/main/java/com/example/jetpackcomposevisualnoteapp/features/navigation/Screen.kt
@@ -4,5 +4,7 @@ sealed class Screen(val route: String) {
     object NoteListScreen : Screen("note_list_screen")
     object NoteEditScreen : Screen("note_edit_screen")
     object NoteAddScreen : Screen("note_add_screen")
+    object NoteDetailScreen : Screen("note_detail_screen")
     object Splash : Screen("splash_screen")
+    object DrawingScreen : Screen("drawing_screen")
 }
